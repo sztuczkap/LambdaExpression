85e60d6fa4f9f56df69adef24c758075441114ff
sztuczkap <p.sztuczka@wp.pl>
2020-12-28 17:27:02 +0100

    wyrażenie lambda

--- src/pl/sztuczkap/Main.java
+++ src/pl/sztuczkap/Main.java
@@ -1,8 +1,25 @@
 package pl.sztuczkap;
 
+import java.util.Comparator;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+/*        // wyrazenie wypisze Hello from lambda
+        () -> System.out.println("Hello from lambda");
+
+        // wyrazenie wypisze pazyste liczby od 0-10
+        () -> {
+            for (int i = 0; i <= 10; i++) {
+                if (i % 2 == 0) {
+                    System.out.println(i);
+                }
+            }
+        };*/
+
+        // wyrazenie zwracajace roznice dlugosci znakow dla 2 zmiennych typu String
+        Comparator<String> comparator = (first, second) -> first.length() - second.length();
+        System.out.println(comparator.compare("pierwszy", "drugi"));
     }
 }
